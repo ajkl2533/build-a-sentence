5e879c5ab3638a1222d43902f8af9f11858cbece
Radek Podrazky <radek.podrazky@oracle.com>
2019-02-28 20:44:52 +0100

    feat(redux): bootstrap redux store (#1)
    * chore(package): add redux-actions@2.6.4
    
    * feat(redux): bootstrap redux store

--- src/index.js
+++ src/index.js
@@ -1,8 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore, combineReducers } from 'redux';
+import { Provider } from 'react-redux';
 
 import './styles/main.scss';
 import App from './App';
+import gameReducer from './game/reducers';
 
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(combineReducers({
+    game: gameReducer,
+  }),
+  typeof window !== 'undefined' 
+    && window.__REDUX_DEVTOOLS_EXTENSION__ 
+    && window.__REDUX_DEVTOOLS_EXTENSION__(),
+)
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>, 
+  document.getElementById('root')
+);
